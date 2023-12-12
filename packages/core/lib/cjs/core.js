2879e6f28e086521631a3e2b7f12746dd506bd51
Mayank Wised <mayank@wisedinc.com>
2023-12-12 12:06:04 +0530

    commit25

--- packages/core/lib/cjs/core.js
+++ packages/core/lib/cjs/core.js
@@ -4008,7 +4008,7 @@ var PdfWriter = function (_a) {
         if (!writing)
             return;
         var x = event.clientX - rectDims.left;
-        var y = event.clientY + window.scrollY + rectDims.top;
+        var y = event.clientY + window.scrollY;
         setCurrentPath("M ".concat(x, " ").concat(y));
         setRedoHistory([]);
         setPathCoordinates([[x, y]]);
